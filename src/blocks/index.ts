176e939960a7bd96754e34cf3a9577fe42b8d71c
Mr. Hasan <hasanshahriar32@gmail.com>
2025-02-19 01:40:58 +0600

    Refactor import paths for Content block to use lowercase directory name

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,12 +1,12 @@
-import { Content, Component as content } from './Content';
-import { Image, Component as image } from './Image';
+import { Content, Component as content } from './content'
+import { Image, Component as image } from './Image'
 
 export const collections = {
   Content,
   Image,
-};
+}
 
 export const components = {
   content,
   image,
-};
+}
