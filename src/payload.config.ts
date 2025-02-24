d1694f34a598e1092b772d9b7dc0bd5036881725
Mr. Hasan <hasanshahriar32@gmail.com>
2025-02-24 09:02:25 +0600

    Update globals in payload configuration to include SocialMedia and Footer

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -25,7 +25,7 @@ export default buildConfig({
     },
   },
   collections: [Users, Media, FormSubmissions, Study, Category, Page],
-  globals: [MegaMenu],
+  globals: [SocialMedia, Footer],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
